46461d3c332a6b562a9b0334bdb3b253f6357206
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-01-25 11:51:14 -0800

    Initial commit and added framework for algorithms

---
+++ MultAlgo3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int multA(int inMultiplicand, int inMultiplier) {
+
+    // Long multiplication
+
+    vector<int> multiplicand;
+    vector<int> multiplier;
+
+    while (inMultiplicand > 0) {
+
+    }
+}
+
+int multB(int inMultiplicand, int inMultiplier) {
+
+}
+
+int multC(int inMultiplicand, int inMultiplier) {
+
+}
+
+int main() {
+    cout << "Eisig Liang - 26 Jan. 2025" << endl;
+    cout << multA(281, 2145) << endl;
+    cout << multB(281, 2145) << endl;
+    cout << multC(281, 2145) << endl;
+}
