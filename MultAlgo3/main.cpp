c64cd005a03b141f13f24e77cb3bea64e465f95e
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-01-25 14:04:13 -0800

    Extra files that popped out of nowhere and I'm scared to delete

--- MultAlgo3/main.cpp
+++ MultAlgo3/main.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 using namespace std;
 
 int multA(int inMultiplicand, int inMultiplier) {
 
     // Long multiplication
 
-    vector<int> multiplicand;
-    vector<int> multiplier;
+    stack<int> multiplicand;
+    stack<int> multiplier;
+    int result = 0;
 
-    while (inMultiplicand > 0) {
-
+    // Get factor digits
+    while (inMultiplicand != 0) {
+        multiplicand.push(inMultiplicand % 10);
+        inMultiplicand /= 10;
     }
+
+    while (inMultiplier != 0) {
+        multiplier.push(inMultiplier % 10);
+        inMultiplier /= 10;
+    }
+
+    
+
+    return 0;
 }
 
-int multB(int inMultiplicand, int inMultiplier) {
-
-}
-
-int multC(int inMultiplicand, int inMultiplier) {
-
-}
+//int multB(int inMultiplicand, int inMultiplier) {
+//
+//}
+//
+//int multC(int inMultiplicand, int inMultiplier) {
+//
+//}
 
 int main() {
     cout << "Eisig Liang - 26 Jan. 2025" << endl;
-    cout << multA(281, 2145) << endl;
+    multA(2, 4);
+    /*cout << multA(281, 2145) << endl;
     cout << multB(281, 2145) << endl;
-    cout << multC(281, 2145) << endl;
+    cout << multC(281, 2145) << endl;*/
 }
